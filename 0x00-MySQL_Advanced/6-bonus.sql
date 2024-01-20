142f3567f038f92d8a311364e0a31f747f4e69cd
Abdelhay1212 <110763003+Abdelhay1212@users.noreply.github.com>
2024-01-20 04:31:58 +0000

    Write code in SQL is a nice level up!

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -3,25 +3,25 @@
 
 DELIMITER $$
 CREATE PROCEDURE AddBonus(
-    IN p_user_id INT,
-    IN p_project_name varchar(255),
-    IN p_score INT
+    IN user_id INT,
+    IN project_name varchar(255),
+    IN score INT
 )
 BEGIN
     DECLARE project_id INT;
 
     SELECT id INTO project_id
-    FROM users
-    WHERE id = p_user_id;
+    FROM projects
+    WHERE name = project_name;
 
     IF project_id IS NULL THEN
         INSERT INTO projects(name)
-        VALUES (p_project_name);
+        VALUES (project_name);
         SET project_id = LAST_INSERT_ID();
     END IF;
 
     INSERT INTO corrections(user_id, project_id, score)
-    VALUES (p_user_id, project_id, p_score);
+    VALUES (user_id, project_id, score);
 
 END $$
 DELIMITER ;
