6e9151e32c291f9f0ce240329118c454ff061a56
Abdelhay1212 <110763003+Abdelhay1212@users.noreply.github.com>
2024-01-20 04:21:06 +0000

    Write code in SQL is a nice level up!

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,27 @@
+-- SQL script that creates a stored procedure AddBonus
+-- that adds a new correction for a student.
+
+DELIMITER $$
+CREATE PROCEDURE AddBonus(
+    IN p_user_id INT,
+    IN p_project_name varchar(255),
+    IN p_score INT
+)
+BEGIN
+    DECLARE project_id INT;
+
+    SELECT id INTO project_id
+    FROM users
+    WHERE id = p_user_id;
+
+    IF project_id IS NULL THEN
+        INSERT INTO projects(name)
+        VALUES (p_project_name);
+        SET project_id = LAST_INSERT_ID();
+    END IF;
+
+    INSERT INTO corrections(user_id, project_id, score)
+    VALUES (p_user_id, project_id, p_score);
+
+END $$
+DELIMITER ;
