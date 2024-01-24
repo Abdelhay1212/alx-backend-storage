83d1c616f9b29a5abcb552203b7d923076032578
Abdelhay1212 <110763003+Abdelhay1212@users.noreply.github.com>
2024-01-24 07:28:52 +0000

    log stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -6,35 +6,25 @@ from pymongo import MongoClient
 def stats_about_nginx_logs(collection):
     ''' stats about Nginx logs '''
 
-    # count documents in nginx collection
+    # count documents in nginx collection and print it
     num_collections = collection.count_documents({})
+    print(f'{num_collections} logs')
 
-    # count documents in nginx collection for every method
-    get_method = collection.count_documents({'method': 'GET'})
-    post_method = collection.count_documents({'method': 'POST'})
-    put_method = collection.count_documents({'method': 'PUT'})
-    patch_method = collection.count_documents({'method': 'PATCH'})
-    delete_method = collection.count_documents({'method': 'DELETE'})
+    # count documents in nginx collection for every method and print it
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print('Methods:')
+    for method in methods:
+        res = collection.count_documents({'method': method})
+        print(f'    method {method}: {res}')
 
-    # status check
-    num_checks = collection.count_documents({'path': '/status'})
-
-    # print the stats
-    print(f'''
-{num_collections} logs
-Methods:
-    method GET: {get_method}
-    method POST: {post_method}
-    method PUT: {put_method}
-    method PATCH: {patch_method}
-    method DELETE: {delete_method}
-{num_checks} status check''')
+    # count status check and print it out
+    num_checks = collection.count_documents(
+        {'method': 'GET', 'path': '/status'})
+    print(f'{num_checks} status check')
 
 
-# Connect to MongoDB
-client = MongoClient("mongodb://localhost:27017/")
-db = client.logs
-collection = db.nginx
-
-# call the function and print the stats
-stats_about_nginx_logs(collection)
+if __name__ == '__main__':
+    client = MongoClient("mongodb://localhost:27017/")
+    db = client.logs
+    collection = db.nginx
+    stats_about_nginx_logs(collection)
