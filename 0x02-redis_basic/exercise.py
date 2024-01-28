928384777e5dd5fc444764717cd1a7e4a7d30201
Abdelhay1212 <110763003+Abdelhay1212@users.noreply.github.com>
2024-01-28 04:10:23 +0000

    Storing lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -17,6 +17,20 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    ''' call method history '''
+    @wraps(method)
+    def wrapper(self, *args):
+        input_key = f'{method.__qualname__}:inputs'
+        self._redis.rpush(input_key, str(args))
+
+        output_key = f'{method.__qualname__}:outputs'
+        output = method(self, *args)
+        self._redis.rpush(output_key, output)
+        return output
+    return wrapper
+
+
 class Cache:
     ''' cache class '''
 
@@ -26,6 +40,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         ''' store data '''
         key = str(uuid.uuid4())
