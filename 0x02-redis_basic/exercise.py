8483de2eaf791bfac83451447b4081c6c59ca87c
Abdelhay1212 <110763003+Abdelhay1212@users.noreply.github.com>
2024-01-28 04:24:55 +0000

    updated

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -11,7 +11,7 @@ def count_calls(method: Callable) -> Callable:
     ''' count method calls '''
     @wraps(method)
     def wrapper(self, *args, **kwargs):
-        key = f'{method.__qualname__}'
+        key = method.__qualname__
         self._redis.incr(key)
         return method(self, *args, **kwargs)
     return wrapper
