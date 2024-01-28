ca99a20e2b92d3ee7a9ffe0d7c9b26c2fa3bb4ae
Abdelhay1212 <110763003+Abdelhay1212@users.noreply.github.com>
2024-01-28 01:15:08 +0000

    fixed the order of types dislayed

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -14,7 +14,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[int, float, str, bytes]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         ''' store data '''
         key = str(uuid.uuid4())
         self._redis.set(key, data)
